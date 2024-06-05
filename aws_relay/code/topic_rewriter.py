10fe4a71479fb3665817661d712bc3f0a033b794
Greg Hawkridge <ghawkridge@gmail.com>
2024-06-05 10:14:50 +0100

    Fast Forward (#10)
    * Adds Readme File (#8)
    
    * Update config.toml (#9)
    
    Co-authored-by: catia-santos <141425344+catia-santos@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: catia-santos <141425344+catia-santos@users.noreply.github.com>

--- aws_relay/code/topic_rewriter.py
+++ aws_relay/code/topic_rewriter.py
@@ -1,3 +1,25 @@
+# ----------------------------------------------------------------------
+#
+#    AWS Relay -- This digital solution subscribe to a local mqtt broker,
+#    forward the message to an endpoint in AWS.
+#
+#    Copyright (C) 2022  Shoestring and University of Cambridge
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, version 3 of the License.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see https://www.gnu.org/licenses/.
+#
+# ----------------------------------------------------------------------
+
+
 import multiprocessing
 import zmq
 import logging
